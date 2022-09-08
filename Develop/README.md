0074bb205dcd1d47d8139b4e9db71124d0bb7b86
rforaker <rtforaker@gmail.com>
2022-09-07 21:06:37 -0400

    Added test README

---
+++ Develop/README.md
@@ -0,0 +1,39 @@
+# Test
+
+    ## Description
+    
+    aefaf
+    
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    - [Questions](#questions)
+    
+    ## Installation
+    
+    afawf
+    
+    ## Usage
+    
+    awfawf
+    
+    ## License
+    
+     This project is licensed under the MIT license.
+    
+    ---
+    
+    ## How to Contribute
+    
+    awfawf
+    
+    ## Tests
+    awfdaw
+
+    ## Questions?
+    https://github.com/awfafw
+    Email me at awfawf
+    
--- Develop/index.js
+++ Develop/index.js
@@ -75,7 +75,7 @@ inquirer
     },
   ])
   .then((data) => {
-    const filename = `${data.projectTitle.toLowerCase().split(' ').join('')}.txt`;
+    const filename = `README.md`;
     readMe = `# ${data.projectTitle}
 
     ## Description
@@ -98,32 +98,11 @@ inquirer
     
     ${data.usage}
     
-   
-    ## Credits
-    
-    List your collaborators, if any, with links to their GitHub profiles.
-    
-    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-    
-    If you followed tutorials, include links to those here as well.
-    
     ## License
     
      This project is licensed under the ${data.license} license.
     
     ---
-    
-    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-    
-    ## Badges
-    
-    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-    
-    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-    
-    ## Features
-    
-    If your project has a lot of features, list them here.
     
     ## How to Contribute
     
